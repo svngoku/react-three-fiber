f7197f67893c7480d51b8ae169aae19959c4c388
svngoku <marymarty104@gmail.com>
2022-11-01 20:40:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,53 +20,131 @@ const state = proxy({
   },
 })
 
-function Shoe() {
-  const ref = useRef()
-  const snap = useSnapshot(state)
-  // Drei's useGLTF hook sets up draco automatically, that's how it differs from useLoader(GLTFLoader, url)
-  // { nodes, materials } are extras that come from useLoader, these do not exist in threejs/GLTFLoader
-  // nodes is a named collection of meshes, materials a named collection of materials
-  const { nodes, materials } = useGLTF("shoe-draco.glb")
-
-  // Animate model
-  useFrame((state) => {
-    const t = state.clock.getElapsedTime()
-    ref.current.rotation.z = -0.2 - (1 + Math.sin(t / 1.5)) / 20
-    ref.current.rotation.x = Math.cos(t / 4) / 8
-    ref.current.rotation.y = Math.sin(t / 4) / 8
-    ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
-  })
-
-  // Cursor showing current color
-  const [hovered, set] = useState(null)
-  useEffect(() => {
-    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
-    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
-    if (hovered) {
-      document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(cursor)}'), auto`
-      return () => (document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(auto)}'), auto`)
-    }
-  }, [hovered])
-
-  // Using the GLTFJSX output here to wire in app-state and hook up events
+function Model(props) {
+  const { nodes, materials } = useGLTF("/1.glb")
   return (
-    <group
-      ref={ref}
-      dispose={null}
-      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
-      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
-      onPointerMissed={() => (state.current = null)}
-      onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
-      <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+    <group {...props} dispose={null}>
+      <group position={[-2.139541, 0.1, -2.536948]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["0______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[1.270318, 0.1, -3.286821]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["1______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[4.424695, 0.1, -8.415809]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["2______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[5.824391, 0.225, -4.465038]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["3______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.099999, 0.1, -1.862108]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["4______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.474924, 0.1, -1.437222]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["5______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.574896, 0.1, -0.112328]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["6______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.12496, 0.1, 0.917314]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["7______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.099999, 0.1, 1.767097]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["8______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.474924, 0.1, 2.366882]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["9______011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.549907, 0.1, 3.666783]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["10_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.174952, 0.1, 4.666746]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["11_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.099999, 0.1, 6.666278]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["12_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[-2.039539, 0.1, 0.917314]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["13_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[5.74935, 0.35, -3.303688]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["14_____011_1"].geometry} material={materials.Glass___Clear_Fa} material-color="#ffffff" />
+          <mesh geometry={nodes["14_____011_2"].geometry} material={materials.Wood___Pine_Grai} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[-0.19957, -0.075, -0.622668]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["15_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[-0.197303, 0.034391, -0.435486]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["16_____011_1"].geometry} material={materials.Glass___Clear_Fa} material-color="#ffffff" />
+          <mesh geometry={nodes["16_____011_2"].geometry} material={materials.Wood___Pine_Grai} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[-5.499402, -0.250001, -0.780908]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["18_____011_1"].geometry} material={materials.Glass___Clear_Fa} material-color="#ffffff" />
+          <mesh geometry={nodes["18_____011_2"].geometry} material={materials.Wood___Pine_Grai} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[2.186797, -0.125, -4.036697]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["19_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[2.213431, -0.065611, -4.008081]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["20_____011_1"].geometry} material={materials.Glass___Clear_Fa} material-color="#ffffff" />
+          <mesh geometry={nodes["20_____011_2"].geometry} material={materials.Wood___Pine_Grai} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[3.04772, -0.065611, -3.329801]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["21_____011_1"].geometry} material={materials.Glass___Clear_Fa} material-color="#ffffff" />
+          <mesh geometry={nodes["21_____011_2"].geometry} material={materials.Wood___Pine_Grai} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[4.449697, 0.1, -2.586993]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["22_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[-0.914379, -0.133333, 6.266304]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["23_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[2.699998, 0.1, 7.515916]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["24_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[-3.429125, -0.250001, 6.066306]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["25_____011_1"].geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes["25_____011_2"].geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes["25_____011_3"].geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[1.935583, -0.250001, 6.066306]} rotation={[-Math.PI / 2, 0, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes["26_____011_1"].geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes["26_____011_2"].geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+        </group>
+      </group>
+      <group position={[-5.658711, -0.330232, 1.757626]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["27_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[3.049995, -0.292858, -4.314039]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["28_____011"].geometry} material={materials.Paint___Light_Gr} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[1.494634, -1.520059, -0.324249]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh geometry={nodes["29_____011"].geometry} material={materials["fallback Material"]} scale={0.01} material-color="#ffffff" />
+      </group>
+      <group position={[-5.489396, -1.4, 3.24599]} rotation={[0, -Math.PI / 2, 0]}>
+        <group scale={0.01}>
+          <mesh geometry={nodes.FixedWindow001_1.geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes.FixedWindow001_2.geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes.FixedWindow001_3.geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes.FixedWindow001_4.geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+          <mesh geometry={nodes.FixedWindow001_5.geometry} material={materials["fallback Material"]} material-color="#ffffff" />
+        </group>
+      </group>
     </group>
   )
+  useGLTF.preload("/1.glb")
 }
 
 function Picker() {
@@ -82,17 +160,11 @@ function Picker() {
 export default function App() {
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
-        <ambientLight intensity={0.7} />
-        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-        <Suspense fallback={null}>
-          <Shoe />
-          <Environment preset="city" />
-          <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
-        </Suspense>
-        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+      <Canvas shadows camera={{ position: [0, 5, 10], fov: 100 }} size={[`2000px`, `3000px`]}>
+        <Model />
+        <Environment preset="city" />
+        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enable Zoom={false} enablePan={false} enableDamping />
       </Canvas>
-      <Picker />
     </>
   )
 }
